e13a71e1efc1f0acc18cdea657d67609612d495e
unknown <ccz9000@yahoo.com>
2021-11-06 03:11:30 -0300

    3 da manhã, tô cansadão, amanhã eu continuo

---
+++ js/geral.js
@@ -0,0 +1,12 @@
+var usuarios = [[],[],[],[]]
+var logado = false
+
+function cadastro() {
+    var email = document.email.username.value
+    usuarios[4].append(email)
+    location.replace("../paginas/cadastro2.html")
+}
+function finalizar_cad(){
+    //TODO Fazer todo o javascript, queria fazer hoje de verdade, mas já é 3:10 da manhã e eu tô exausto, amanhã eu continuo fechou?
+    //vou subir tudo no git e é isso por hoje
+}
