4d8bbac1e9df9885872a8a513f1a5bd60a3f3bd2
unknown <ccz9000@yahoo.com>
2021-11-06 17:59:16 -0300

    dor.

--- js/geral.js
+++ js/geral.js
@@ -1,12 +1,25 @@
 var usuarios = [[],[],[],[]]
 var logado = false
+var nome_usuario = null
 
 function cadastro() {
     var email = document.email.username.value
-    usuarios[4].append(email)
-    location.replace("../paginas/cadastro2.html")
+    console.log(email)
+    usuarios[3].push(email)
+    console.log(usuarios)
+    window.location.href = "../paginas/cadastro2.html";
 }
 function finalizar_cad(){
-    //TODO Fazer todo o javascript, queria fazer hoje de verdade, mas já é 3:10 da manhã e eu tô exausto, amanhã eu continuo fechou?
-    //vou subir tudo no git e é isso por hoje
+    var login = document.form.nome.value
+    var senha = document.form.senha.value
+    var nome_i = document.form.nome_invocador.value
+    usuarios[0].push(login);
+    usuarios[1].push(senha);
+    usuarios[2].push(nome_i);
+    window.location.href = "../paginas/login.html";
+}
+function login(){
+    alert("autenticado")
+    logado = true
+    window.location.href = "../index.html";
 }
