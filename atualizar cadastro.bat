6cea12883be13ddf750948d9b6c2adc2ebd8cb07
unknown <ccz9000@yahoo.com>
2021-11-11 16:35:42 -0300

    adicionando a branch 404, faltam mais duas branches, a com os comentários e a com o mapinha

---
+++ atualizar cadastro.bat
@@ -0,0 +1,8 @@
+git checkout cadastro
+git add ./imagens
+git add ./css/cadastro.css
+git add ./css/cadastro2.css
+git add ./paginas/cadastro1.html
+git add ./paginas/cadastro2.html
+git commit
+git push
